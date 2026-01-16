bba2d41b03833ebd49a8605ee1df1738ebdad503
TheSlize <r.region-4@yandex.ru>
2026-01-17 01:39:48 +0300

    refactor: added CTM dependency for runClient, made space fluids' ids fixed so that adding fluids to main CE won't fuck up everything
    note: IT WILL FUCK FLUIDS IN EXISTING CE-SPACE WORLDS. BE CAREFUL!

--- src/main/java/com/hbmspace/mixin/mod/hbm/MixinFluids.java
+++ src/main/java/com/hbmspace/mixin/mod/hbm/MixinFluids.java
@@ -7,6 +7,7 @@ import com.hbm.render.misc.EnumSymbol;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,6 +22,14 @@ public abstract class MixinFluids {
     @Final
     protected static List<FluidType> metaOrder;
 
+    @Unique
+    private static FluidType space$createFixed(String name, int color, int p, int f, int r, EnumSymbol symbol, int id) {
+        FluidType fluid = new FluidType(name, color, p, f, r, symbol, name.toLowerCase(java.util.Locale.US), 0xFFFFFF, id, null);
+        fluid.customFluid = false; // since otherwise I won't be able to render corresponding textures for tanks
+
+        return fluid;
+    }
+
     @Inject(
             method = "init",
             at = @At(
@@ -32,18 +41,20 @@ public abstract class MixinFluids {
     private static void hbmextra$registerExtraFluids(CallbackInfo ci) {
         if (Fluids.fromName("EARTHAIR") != Fluids.NONE) return;
 
-        EARTHAIR = new FluidType("EARTHAIR", 0xD1CEEE, 0, 0, 0, EnumSymbol.NONE).addTraits(Fluids.GASEOUS);
-        CCL = new FluidType("CCL", 0x0C3B2F, 0, 0, 0, EnumSymbol.NONE).addTraits(Fluids.LIQUID, new FT_Corrosive(10));
-        CHLOROETHANE = new FluidType("CHLOROETHANE", 0xBBA9A0, 2, 0, 0, EnumSymbol.NONE).addTraits(Fluids.GASEOUS);
-        CBENZ = new FluidType("CBENZ", 0x91C6BB, 0, 0, 0, EnumSymbol.NONE).addTraits(Fluids.LIQUID);
-        VINYL =					new FluidType("VINYL",				0xA2A2A2, 0, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
-        AQUEOUS_COPPER =		new FluidType("AQUEOUS_COPPER",		0x4CC2A2, 0, 0, 0, EnumSymbol.NONE).addTraits(LIQUID, VISCOUS);
-        AQUEOUS_NICKEL =		new FluidType("AQUEOUS_NICKEL",		0xDACEBA, 0, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
-        COPPERSULFATE =			new FluidType("COPPERSULFATE",		0x55E5CF, 0, 0, 0, EnumSymbol.NONE).addTraits(LIQUID, VISCOUS);
-        BRINE =					new FluidType("BRINE",				0xD1A73E, 3, 3, 3, EnumSymbol.NONE).addTraits(LIQUID, VISCOUS);
-        CONGLOMERA =			new FluidType("CONGLOMERA",			0x364D47, 0, 0, 2, EnumSymbol.NONE).addTraits(LIQUID, VISCOUS);
-        HGAS =					new FluidType("HGAS",				0x999368, 0, 0, 0, EnumSymbol.ACID).addTraits(GASEOUS,  new FT_Corrosive(120));
-        HALOLIGHT =				new FluidType("HALOLIGHT",			0xB6F9CF, 0, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
+        int idCounter = 4000;
+
+        EARTHAIR = space$createFixed("EARTHAIR", 0xD1CEEE, 0, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(Fluids.GASEOUS);
+        CCL = space$createFixed("CCL", 0x0C3B2F, 0, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(Fluids.LIQUID, new FT_Corrosive(10));
+        CHLOROETHANE = space$createFixed("CHLOROETHANE", 0xBBA9A0, 2, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(Fluids.GASEOUS);
+        CBENZ = space$createFixed("CBENZ", 0x91C6BB, 0, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(Fluids.LIQUID);
+        VINYL = space$createFixed("VINYL", 0xA2A2A2, 0, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(LIQUID);
+        AQUEOUS_COPPER = space$createFixed("AQUEOUS_COPPER", 0x4CC2A2, 0, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(LIQUID, VISCOUS);
+        AQUEOUS_NICKEL = space$createFixed("AQUEOUS_NICKEL", 0xDACEBA, 0, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(LIQUID);
+        COPPERSULFATE = space$createFixed("COPPERSULFATE", 0x55E5CF, 0, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(LIQUID, VISCOUS);
+        BRINE = space$createFixed("BRINE", 0xD1A73E, 3, 3, 3, EnumSymbol.NONE, idCounter++).addTraits(LIQUID, VISCOUS);
+        CONGLOMERA = space$createFixed("CONGLOMERA", 0x364D47, 0, 0, 2, EnumSymbol.NONE, idCounter++).addTraits(LIQUID, VISCOUS);
+        HGAS = space$createFixed("HGAS", 0x999368, 0, 0, 0, EnumSymbol.ACID, idCounter++).addTraits(GASEOUS, new FT_Corrosive(120));
+        HALOLIGHT = space$createFixed("HALOLIGHT", 0xB6F9CF, 0, 0, 0, EnumSymbol.NONE, idCounter++).addTraits(LIQUID);
 
         metaOrder.add(EARTHAIR);
         metaOrder.add(CCL);
